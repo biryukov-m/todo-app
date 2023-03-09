fc0b1f01e09950960703b2cedf558c02910b31a9
Maxym Biriukov <biryukov.mm@gmail.com>
2023-03-09 13:23:51 +0200

    ref: Remove uunecessary code from boilerplate

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -3,9 +3,6 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
-import axios from 'axios';
 import AppRouter from './routes';
 import connectDB from './config/database';
 
@@ -21,32 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 router.init();
-
-// TODO: Move that to model GraphQL
-const schema = buildSchema(`
-  type Query {
-    todos: String
-  }
-`);
-
-// TODO: Create graphQL controller
-const rootValue = {
-  todos: async () => {
-    // TODO: Create http service for that
-    const todos = await axios.get('http://localhost:5000/api/todos');
-    return todos.data;
-  }
-};
-
-// TODO: Move that to router init function ONLY AFTER MAIN PART OF APP
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true
-  })
-);
 
 const port = app.get('port');
 // eslint-disable-next-line no-console
--- packages/backend/src/types/request.type.ts
+++ packages/backend/src/types/request.type.ts
@@ -2,6 +2,7 @@
 import { ParamsDictionary } from 'express-serve-static-core';
 import { Request } from 'express';
 
+// TODO: add generic request query
 export interface IRequest<T, P extends ParamsDictionary> extends Request {
   body: T;
   params: P;
