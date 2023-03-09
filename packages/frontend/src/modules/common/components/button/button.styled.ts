82382eb833a1e1bff47cee02a3b4d2877c9adf3c
Maxym Biriukov <biryukov.mm@gmail.com>
2023-03-09 20:52:07 +0200

    feat: Add button common component

---
+++ packages/frontend/src/modules/common/components/button/button.styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { FONTS, COLORS, SPACES } from '../../../theme';
+
+interface IButton {
+  large?: boolean;
+}
+export const Button = styled.button<IButton>`
+  outline: 0;
+  text-decoration: none;
+  padding: ${SPACES.xs} ${SPACES.s};
+  background-color: ${COLORS.white};
+  font-family: ${FONTS.FAMILIES.normal};
+  font-size: ${FONTS.SIZES.m};
+  color: ${COLORS.black};
+  cursor: pointer;
+  border: 1px solid ${COLORS.black};
+  border-radius: 3px;
+  box-shadow: 0px 13px 40px -12px rgba(0, 0, 0, 0.35);
+  transition: all 0.3s ease 0s;
+  &:hover,
+  &.clicked {
+    background-color: ${COLORS.primary};
+    box-shadow: 0px 13px 40px -12px ${COLORS.primary};
+  }
+`;
